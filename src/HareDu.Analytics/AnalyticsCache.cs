7c4593f8810244564a2bf75892e4636e76f008d4
Albert Hives <ahives@hotmail.com>
2020-01-04 11:48:34 -0800

    change license info

--- src/HareDu.Analytics/AnalyticsCache.cs
+++ src/HareDu.Analytics/AnalyticsCache.cs
@@ -1,4 +1,4 @@
-// Copyright 2013-2019 Albert L. Hives
+// Copyright 2013-2020 Albert L. Hives
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,6 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
+// limitations under the License.
 namespace HareDu.Analytics
 {
     using System.Collections.Generic;
