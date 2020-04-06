377931fdfa5993c368c523f0ed090e9ad6b49636
Albert Hives <ahives@hotmail.com>
2020-04-05 20:16:58 -0700

    fixed test

--- src/IntegrationTesting/HareDu.IntegrationTesting.Snapshots/QueueSnapshotTests.cs
+++ src/IntegrationTesting/HareDu.IntegrationTesting.Snapshots/QueueSnapshotTests.cs
@@ -33,6 +33,7 @@ namespace HareDu.IntegrationTesting.Snapshots
         public void Init()
         {
             _container = new ContainerBuilder()
+                .AddHareDuConfiguration($"{TestContext.CurrentContext.TestDirectory}/haredu.yaml")
                 .AddHareDu()
                 .AddHareDuSnapshot()
                 .Build();
